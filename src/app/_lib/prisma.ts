a07d7aed5e660c6d76a279283938f430cc31ede7
Joaobatista22 <joaobatiista.ds@gmail.com>
2024-09-12 23:42:50 -0300

    prisma

--- src/app/_lib/prisma.ts
+++ src/app/_lib/prisma.ts
@@ -2,19 +2,13 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-	// eslint-disable-next-line no-var
 	var cachedPrisma: PrismaClient | undefined;
 }
 
-let prisma: PrismaClient;
+const prisma = global.cachedPrisma || new PrismaClient();
 
 if (process.env.NODE_ENV === "production") {
-	prisma = new PrismaClient();
-} else {
-	if (!global.cachedPrisma) {
-		global.cachedPrisma = new PrismaClient();
-	}
-	prisma = global.cachedPrisma;
+	global.cachedPrisma = prisma;
 }
 
 export const db = prisma;
